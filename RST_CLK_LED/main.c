ca151809bf0f96d846419f7ba14da42a25925bc2
User <tmyaldzin13@gmail.com>
2025-11-12 15:35:36 +0300

    Edit LED_Blink func

--- RST_CLK_LED/main.c
+++ RST_CLK_LED/main.c
@@ -1,27 +1,6 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Milandr Application Team
-  * @version V2.0.4
-  * @date    30/05/2024
-  * @brief   Main program body.
-  ******************************************************************************
-  * <br><br>
-  *
-  * THE PRESENT FIRMWARE IS FOR GUIDANCE ONLY. IT AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING MILANDR'S PRODUCTS IN ORDER TO FACILITATE
-  * THE USE AND SAVE TIME. MILANDR SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES RESULTING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR A USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2025 Milandr</center></h2>
-  */
-
 /* Includes ------------------------------------------------------------------*/
 #include "MDR32FxQI_port.h"
 #include "MDR32FxQI_rst_clk.h"
-#include "MDR32FxQI_eeprom.h"
 #include "MDR32FxQI_bkp.h"
 
 /** @addtogroup __MDR32FxQI_StdPeriph_Examples MDR32FxQI StdPeriph Examples
@@ -35,21 +14,64 @@
 /** @addtogroup RST_CLK_CPU_Clock_MDR32F9Q2I RST_CLK_CPU_Clock
   * @{
   */
-
-/* Private define ------------------------------------------------------------*/
-#define BLINK_NUM 5
-#define BLINK_DELAY 200000
-
-#define LED1            PORT_Pin_7
+#define LED_PORT     MDR_PORTB
+#define LED1         PORT_Pin_7
 
 /* Private variables ---------------------------------------------------------*/
-RST_CLK_FreqTypeDef CLK_FrequencyStructure;
 PORT_InitTypeDef PORT_InitStructure;
 
 /* Private function prototypes -----------------------------------------------*/
 void Delay(__IO uint32_t nCount);
-
+void ClockInit(void);
+void LEDInit(void);
+void LEDOn(uint32_t LED_Num);
+void LEDOff(uint32_t LED_Num);
+void BlinkLED1(uint32_t del);
 /* Private functions ---------------------------------------------------------*/
+void ClockInit(void){
+    RST_CLK_DeInit();
+    SystemCoreClockUpdate();
+
+		RST_CLK_HSEconfig(RST_CLK_HSE_ON);
+	
+		if (RST_CLK_HSEstatus() != SUCCESS) {
+				while(1);
+		}
+	
+		RST_CLK_CPU_PLLconfig(RST_CLK_CPU_PLLsrcHSEdiv1, RST_CLK_CPU_PLLmul4);
+	
+	  RST_CLK_CPU_PLLcmd(ENABLE);
+
+    // Wait for CPU PLL to lock
+    if (RST_CLK_CPU_PLLstatus() != SUCCESS) {        
+        /* Trap if PLL is not successfully locked */        
+        while (1)
+            ;    
+    }
+    // Set CPU clock prescaler to 1 (no division)
+    RST_CLK_CPUclkPrescaler(RST_CLK_CPUclkDIV1);
+
+    // Enable CPU PLL as the clock source
+    RST_CLK_CPU_PLLuse(ENABLE);
+
+    // Select CPU clock C3 (PLL output as the CPU clock source)
+    RST_CLK_CPUclkSelection(RST_CLK_CPUclkCPU_C3);
+	
+		RST_CLK_PCLKcmd((RST_CLK_PCLK_RST_CLK | RST_CLK_PCLK_SSP1 | RST_CLK_PCLK_SSP2), ENABLE);
+		RST_CLK_PCLKcmd((RST_CLK_PCLK_PORTB), ENABLE);
+}
+void LEDInit(void) {
+	
+		PORT_DeInit(LED_PORT);
+		PORT_StructInit(&PORT_InitStructure);
+    PORT_InitStructure.PORT_Pin   = (LED1);
+    PORT_InitStructure.PORT_OE    = PORT_OE_OUT;
+    PORT_InitStructure.PORT_FUNC  = PORT_FUNC_PORT;
+    PORT_InitStructure.PORT_MODE  = PORT_MODE_DIGITAL;
+    PORT_InitStructure.PORT_SPEED = PORT_SPEED_SLOW;		
+		
+		PORT_Init(MDR_PORTB, &PORT_InitStructure);
+}
 
 void LEDOn(uint32_t LED_Num)
 {
@@ -61,99 +83,23 @@ void LEDOff(uint32_t LED_Num)
     PORT_ResetBits(MDR_PORTB, LED_Num);
 }
 
-/**
-  * @brief  Blink with LED1
-  * @param  num - blinks number
-  * @param  del - delay
-  * @retval None
-  */
-void BlinkLED1(uint32_t num, uint32_t del)
+void BlinkLED1(uint32_t del)
 {
-    uint32_t cnt;
-
-    for ( cnt = 0; cnt < num; cnt++)
-    {
-        LEDOn(LED1);
-        Delay(del);
-        LEDOff(LED1);
-        Delay(del);
-    }
+     LEDOn(LED1);
+     Delay(del);
+     LEDOff(LED1);
+     Delay(del);
 }
-
-/**
-  * @brief  Indicate error condition with LED3
-  * @param  None
-  * @retval None
-  */
-void IndicateError(void)
-{
-    /* Switch LED3 on and off in case of error */
-    LEDOn(LED1);
-    Delay(BLINK_DELAY);
-    LEDOff(LED1);
-}
-
-/**
-  * @brief  Main program.
-  * @param  None
-  * @retval None
-  */
 int main(void)
 {
-	  RST_CLK_DeInit();
-    SystemCoreClockUpdate();
+		ClockInit();
+		LEDInit();
 	
-		RST_CLK_HSEconfig(RST_CLK_HSE_ON);
-	
-		if (RST_CLK_HSEstatus() != SUCCESS) {
-				while(1);
-		}
-	
-		RST_CLK_CPU_PLLconfig(RST_CLK_CPU_PLLsrcHSEdiv2, RST_CLK_CPU_PLLmul4);
-	
-	  RST_CLK_CPU_PLLcmd(ENABLE);
-
-    // Wait for CPU PLL to lock
-    if (RST_CLK_CPU_PLLstatus() != SUCCESS) {        
-        /* Trap if PLL is not successfully locked */        
-        while (1)
-            ;    
-    }
-
-    // Set CPU clock prescaler to 1 (no division)
-    RST_CLK_CPUclkPrescaler(RST_CLK_CPUclkDIV1);
-
-    // Enable CPU PLL as the clock source
-    RST_CLK_CPU_PLLuse(ENABLE);
-
-    // Select CPU clock C3 (PLL output as the CPU clock source)
-    RST_CLK_CPUclkSelection(RST_CLK_CPUclkCPU_C3);
-	
-		RST_CLK_PCLKcmd((RST_CLK_PCLK_RST_CLK | RST_CLK_PCLK_SSP1 | RST_CLK_PCLK_SSP2), ENABLE);
-		RST_CLK_PCLKcmd((RST_CLK_PCLK_PORTF | RST_CLK_PCLK_PORTD | RST_CLK_PCLK_PORTC | RST_CLK_PCLK_PORTA| RST_CLK_PCLK_PORTB), ENABLE);
-		
-		PORT_DeInit(MDR_PORTB);
-		
-		PORT_StructInit(&PORT_InitStructure);
-    PORT_InitStructure.PORT_Pin   = (PORT_Pin_7);
-    PORT_InitStructure.PORT_OE    = PORT_OE_OUT;
-    PORT_InitStructure.PORT_FUNC  = PORT_FUNC_PORT;
-    PORT_InitStructure.PORT_MODE  = PORT_MODE_DIGITAL;
-    PORT_InitStructure.PORT_SPEED = PORT_SPEED_SLOW;		
-		
-		PORT_Init(MDR_PORTB, &PORT_InitStructure);
-		//BlinkLED1(5, 1000);
 		while(1) {
-			BlinkLED1(5, 100000);
-			Delay(2000000);
+			BlinkLED1(1000000);
 		};
 }
 
-/**
-  * @brief  Executes delay loop.
-  * @param  nCount: specifies the delay.
-  * @retval None.
-  */
 void Delay(__IO uint32_t nCount)
 {
     for (; nCount != 0; nCount--);
